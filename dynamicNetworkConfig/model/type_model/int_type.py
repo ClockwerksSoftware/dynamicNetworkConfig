63af2635f85cbb3b55e19eeb8bd442edddfcbf6d
Benjamen Meyer <bm_witness@yahoo.com>
2017-02-18 23:43:32 -0500

    Enhancement: Test IntType Model

--- dynamicNetworkConfig/model/type_model/int_type.py
+++ dynamicNetworkConfig/model/type_model/int_type.py
@@ -10,6 +10,7 @@ class IntType(BaseType):
     type_name = six.text_type('integer')
     MIN_VALUE = ((sys.maxsize * -1) - 1)
     MAX_VALUE = sys.maxsize
+    DEFAULT_VALUE = 0
 
     @classmethod
     def isInstance(cls, value):
@@ -18,13 +19,16 @@ class IntType(BaseType):
     def __init__(self, value, minimum, maximum, defaultValue=None,
                  readOnly=False):
         if defaultValue is None:
-            defaultValue = 0
+            defaultValue = self.DEFAULT_VALUE
 
         if minimum is None:
-            minimum = self.MIN_VALUE
+            if readOnly is True:
+                minimum = maximum if maximum is not None else minimum
+            else:
+                minimum = self.MIN_VALUE
 
         if maximum is None:
-            if readOnly:
+            if readOnly is True:
                 maximum = minimum
             else:
                 maximum = self.MAX_VALUE
@@ -43,10 +47,10 @@ class IntType(BaseType):
         assert (self.maximum >= self.minimum)
 
     def isMinimum(self, *args, **kwargs):
-        return (len(self.value) == self.minimum)
+        return (self.value == self.minimum)
 
     def isMaximum(self, *args, **kwargs):
-        return (len(self.value) == self.maximum)
+        return (self.value == self.maximum)
 
     def isDefault(self, *args, **kwargs):
         return (self.value == self.default)
@@ -55,7 +59,7 @@ class IntType(BaseType):
         assert isinstance(other, type(self))
         return (self.value > other.value)
 
-    def isGreaterThanOrEqual(self, *args, **kwargs):
+    def isGreaterThanOrEqual(self, other, *args, **kwargs):
         assert isinstance(other, type(self))
         return (self.value >= other.value)
 
@@ -63,10 +67,10 @@ class IntType(BaseType):
         assert isinstance(other, type(self))
         return (self.value == other.value)
 
-    def isLessThanOrEqual(self, *args, **kwargs):
+    def isLessThanOrEqual(self, other, *args, **kwargs):
         assert isinstance(other, type(self))
         return (self.value <= other.value)
 
-    def isLessThan(self, *args, **kwargs):
+    def isLessThan(self, other, *args, **kwargs):
         assert isinstance(other, type(self))
         return (self.value < other.value)
